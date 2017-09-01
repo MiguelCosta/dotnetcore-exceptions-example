0e06fd91a561ba2fe3d1db16a662a3baccadff70
Miguel Costa <miguelpintodacosta@gmail.com>
2017-09-02 00:54:13 +0100

    Add vegeta

---
+++ vegeta/attack.bat
@@ -0,0 +1,26 @@
+@echo off
+
+set duration = "3s"
+set rate = 100
+
+echo ####################### STATUS CODE 200 #######################
+
+echo NO EXCEPTIONS
+vegeta.exe attack -targets no-exceptions-success.target -duration %duration % -rate %rate % | vegeta.exe report
+
+echo WITH EXCEPTIONS
+vegeta.exe attack -targets with-exceptions-success.target -duration %duration % -rate %rate % | vegeta.exe report
+
+echo ###############################################################
+
+
+
+echo ####################### STATUS CODE 400 #######################
+
+echo NO EXCEPTIONS
+vegeta.exe attack -targets no-exceptions-fail.target -duration %duration % -rate %rate % | vegeta.exe report
+
+echo WITH EXCEPTIONS
+vegeta.exe attack -targets with-exceptions-fail.target -duration %duration % -rate %rate % | vegeta.exe report
+
+echo ###############################################################
